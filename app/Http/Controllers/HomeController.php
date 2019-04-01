93f4ec08377b4820ca3cd1055c2359b8df19e097
stefandehaan <dastepd@gmail.com>
2019-04-01 09:44:38 +0200

    initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
+        $role = Role::findById(1);
+        $permission = Permission::findById(1);
+        $role->givePermissionTo($permission);
+        return view('home');
+    }
+}
